2bb115c89c87535493343d1de089c1228c9e8e6b
at3158 <tk290@tkfood.com.tw>
2022-05-20 15:48:19 +0800

    ADD

--- LeetCode/Form1.cs
+++ LeetCode/Form1.cs
@@ -16,5 +16,57 @@ namespace LeetCode
         {
             InitializeComponent();
         }
+
+        #region
+
+        public void LongestCommonPrefix()
+        {
+            string[] str = new string[] { "my", "myname", "mys" };
+            string res = GetLongestCommonPrefix(str);
+
+            MessageBox.Show(res);
+        }
+
+        private static string GetLongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            if (strs.Length == 1) return strs[0];
+            var min = int.MaxValue;
+            foreach (var item in strs)
+            {
+                if (item.Length < min)
+                {
+                    min = item.Length;
+                }
+            }
+            var index = -1;
+            for (int i = 0; i < min; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != strs[0][i])
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                    else
+                    {
+                        index = i;
+                    }
+                }
+            }
+            return strs[0].Substring(0, index + 1);
+        }
+
+        #endregion
+
+        #region
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LongestCommonPrefix();
+        }
+
+
+        #endregion
+
     }
 }
